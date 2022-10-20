991dc685b1a952695b3b0c0104ef96751e7503e5
Joaquin <95145404+joacoabraldes@users.noreply.github.com>
2022-10-20 13:00:13 -0300

    ...

--- App.js
+++ App.js
@@ -1,27 +1,6 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button } from "react-native";
-
-
-
-
+import { StyleSheet } from "react-native";
 import Start from "./StartStack";
 
-
-function HomeScreen() {
-  return (
-    <View style={styles.container}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
-
-function LoginScreen() {
-  return (
-    <View style={styles.container}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
 export default function App() {
   
   return (
