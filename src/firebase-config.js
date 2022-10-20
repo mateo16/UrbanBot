1dea41133fd77b0543bbd0a4bdda8896a293cede
Joaquin <95145404+joacoabraldes@users.noreply.github.com>
2022-10-20 12:58:31 -0300

    .

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,10 +1,3 @@
-// Import the functions you need from the SDKs you need
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
   apiKey: "AIzaSyBjfB-5rfNtYYa1lh1Ogw7NeJSIRNDiy9M",
   authDomain: "urbanbot-467bd.firebaseapp.com",
