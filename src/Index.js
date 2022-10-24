706ae8f1885285aff6c1a929c06db618d3b9b8ea
Joaquin <95145404+joacoabraldes@users.noreply.github.com>
2022-10-24 17:19:24 -0300

    ....

--- src/Index.js
+++ src/Index.js
@@ -13,89 +13,121 @@ import { useNavigation } from "@react-navigation/native";
 import dayjs from "dayjs";
 import ModalPicker from "./ModalPicker";
 import React, { useEffect, useState } from "react";
+import * as BackgroundFetch from 'expo-background-fetch';
+import * as TaskManager from 'expo-task-manager';
+
+const TASK_NAME = "BOOK_TASK"
+
+const currentDate = dayjs().format("YYYYMMDD");
+
+
+//console.log(today.getDay());
+//console.log(user);
+
+let userId = "1858b64d-40bd-47ee-8025-547e68833fcb";
+
+var myHeaders = new Headers({
+  Authorization:
+    "Bearer MjAyMjEwMTUxOTQwMjh8NmRmNDJiNzM0NWIzNDA1Yjg5NWU0YjY2YzRlMGY3Y2J8ZWMxZDM4ZDdkMzU5NDhkMGE2MGNkOGMwYjhmYjlkZjl8NnxBcmdlbnRpbmEgU3RhbmRhcmQgVGltZXxlcy1FU3wxODU4YjY0ZDQwYmQ0N2VlODAyNTU0N2U2ODgzM2ZjYnx8fHwxfDF8MHwxMDB8fHw1OHw1NTg1fDB8Y29tLm15d2VsbG5lc3M1.430C60AE825FD3A319D5A9B7BDD3A9CC37E3FDBAA342536197BBB8CDF3B95EEC5B05EC8AF7406DBA2F19704BD083850C1E42C7EE868BB5A965973660FE7E94F7",
+  "Content-Type": "application/json",
+  Cookie:
+    "_mwapps=ec1d38d7-d359-48d0-a60c-d8c0b8fb9df9|MjAyMjEwMTUyMjAzMDd8NmRmNDJiNzM0NWIzNDA1Yjg5NWU0YjY2YzRlMGY3Y2J8ZWMxZDM4ZDdkMzU5NDhkMGE2MGNkOGMwYjhmYjlkZjl8NnxBcmdlbnRpbmEgU3RhbmRhcmQgVGltZXxlcy1FU3wxODU4YjY0ZDQwYmQ0N2VlODAyNTU0N2U2ODgzM2ZjYnx8fHwxfDF8MHwxMDB8fHw1OHw1NTg1fDB8Y29tLm15d2VsbG5lc3M1.79E7F833F6F53DC7CA86927EFACE10D42B1FFE178894D5D3287E5D0AE87C8345B9D61559B5FD3442BAD58600120B83FDF64D7357C111B4698E593ADD45BEA661",
+});
+
+var raw = JSON.stringify({
+  timeScope: "Custom",
+  dateLimit: 0,
+  eventType: "Class",
+  dateStart: currentDate,
+});
+var raw2 = JSON.stringify({
+  station: "9",
+  userId: userId,
+  partitionDate: currentDate,
+});
+
+var requestOptions = {
+  method: "POST",
+  headers: myHeaders,
+  body: raw,
+  redirect: "follow",
+};
+var requestOptions2 = {
+  method: "POST",
+  headers: myHeaders,
+  body: raw2,
+  redirect: "follow",
+};
+
+async function getClasses() {
+  try {
+    let res = await fetch(
+      "https://services.mywellness.com/Core/Facility/1c00b0c2-d2b7-46fe-bb98-6eef3be0ed7c/SearchCalendarEvents?_c=es-AR",
+      requestOptions
+    );
+    return await res.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
+async function book(id) {
+  fetch(
+    "https://services.mywellness.com/core/calendarevent/" +
+      id +
+      "/book?_c=es-AR",
+    requestOptions2
+  )
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.log("error", error));
+}
+function getId(classes) {
+  let clases = classes.data.eventItems;
+  let laclase = clases.find((o) => {
+    if (o.startHour === 18 && o.name === "FORCE 6") {
+      return true;
+    }
+  });
+  return laclase.id;
+}
+
+async function reservar() {
+  var today = new Date();
+  if(today.getDay() === 1 ){console.log("Lunes " + today.getMinutes());}
+
+  let classes = await getClasses();
+  let id = getId(classes);
+  book(id);
+}
+
+TaskManager.defineTask(TASK_NAME, () => {
+  try {
+    reservar();
+    const receivedNewData = "Simulated fetch " + Math.random()
+    console.log("My task ", receivedNewData)
+    return receivedNewData
+      ? BackgroundFetch.Result.NewData
+      : BackgroundFetch.Result.NoData
+  } catch (err) {
+    console.log("Error in task ", err)
+    return "Fallo"
+  }
+})
 
 export default function Home({route}) {
 
   const user = route.params;
 
-  const currentDate = dayjs().format("YYYYMMDD");
-
-  var today = new Date();
-
-  //console.log(today.getDay());
-  //console.log(user);
-
-  let userId = "1858b64d-40bd-47ee-8025-547e68833fcb";
-
-  var myHeaders = new Headers({
-    Authorization:
-      "Bearer MjAyMjEwMTUxOTQwMjh8NmRmNDJiNzM0NWIzNDA1Yjg5NWU0YjY2YzRlMGY3Y2J8ZWMxZDM4ZDdkMzU5NDhkMGE2MGNkOGMwYjhmYjlkZjl8NnxBcmdlbnRpbmEgU3RhbmRhcmQgVGltZXxlcy1FU3wxODU4YjY0ZDQwYmQ0N2VlODAyNTU0N2U2ODgzM2ZjYnx8fHwxfDF8MHwxMDB8fHw1OHw1NTg1fDB8Y29tLm15d2VsbG5lc3M1.430C60AE825FD3A319D5A9B7BDD3A9CC37E3FDBAA342536197BBB8CDF3B95EEC5B05EC8AF7406DBA2F19704BD083850C1E42C7EE868BB5A965973660FE7E94F7",
-    "Content-Type": "application/json",
-    Cookie:
-      "_mwapps=ec1d38d7-d359-48d0-a60c-d8c0b8fb9df9|MjAyMjEwMTUyMjAzMDd8NmRmNDJiNzM0NWIzNDA1Yjg5NWU0YjY2YzRlMGY3Y2J8ZWMxZDM4ZDdkMzU5NDhkMGE2MGNkOGMwYjhmYjlkZjl8NnxBcmdlbnRpbmEgU3RhbmRhcmQgVGltZXxlcy1FU3wxODU4YjY0ZDQwYmQ0N2VlODAyNTU0N2U2ODgzM2ZjYnx8fHwxfDF8MHwxMDB8fHw1OHw1NTg1fDB8Y29tLm15d2VsbG5lc3M1.79E7F833F6F53DC7CA86927EFACE10D42B1FFE178894D5D3287E5D0AE87C8345B9D61559B5FD3442BAD58600120B83FDF64D7357C111B4698E593ADD45BEA661",
-  });
-
-  var raw = JSON.stringify({
-    timeScope: "Custom",
-    dateLimit: 0,
-    eventType: "Class",
-    dateStart: currentDate,
-  });
-  var raw2 = JSON.stringify({
-    station: "9",
-    userId: userId,
-    partitionDate: currentDate,
-  });
-
-  var requestOptions = {
-    method: "POST",
-    headers: myHeaders,
-    body: raw,
-    redirect: "follow",
-  };
-  var requestOptions2 = {
-    method: "POST",
-    headers: myHeaders,
-    body: raw2,
-    redirect: "follow",
-  };
-
-  async function getClasses() {
+  RegisterBackgroundTask = async () => {
+    console.log("Registering background task");
     try {
-      let res = await fetch(
-        "https://services.mywellness.com/Core/Facility/1c00b0c2-d2b7-46fe-bb98-6eef3be0ed7c/SearchCalendarEvents?_c=es-AR",
-        requestOptions
-      );
-      return await res.json();
-    } catch (error) {
-      console.log(error);
+      await BackgroundFetch.registerTaskAsync(TASK_NAME, {
+        minimumInterval: 5, // seconds,
+      })
+      console.log("Task registered")
+    } catch (err) {
+      console.log("Task Register failed:", err)
     }
-  }
-  async function book(id) {
-    fetch(
-      "https://services.mywellness.com/core/calendarevent/" +
-        id +
-        "/book?_c=es-AR",
-      requestOptions2
-    )
-      .then((response) => response.text())
-      .then((result) => console.log(result))
-      .catch((error) => console.log("error", error));
-  }
-  function getId(classes) {
-    let clases = classes.data.eventItems;
-    let laclase = clases.find((o) => {
-      if (o.startHour === 18 && o.name === "FORCE 6") {
-        return true;
-      }
-    });
-    return laclase.id;
-  }
-
-  async function reservar() {
-    let classes = await getClasses();
-    let id = getId(classes);
-    book(id);
   }
 
   const navigation = useNavigation();
@@ -154,6 +186,12 @@ export default function Home({route}) {
             <Text style={styles.textReserva} onPress={() => reservar()}>
               {" "}
               Reservar{" "}
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.botonReserva}>
+            <Text style={styles.textReserva} onPress={() => RegisterBackgroundTask()}>
+              {" "}
+              Empezar Bot{" "}
             </Text>
           </TouchableOpacity>
           <TouchableOpacity>
